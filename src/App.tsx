05a832c966567e7aa3cdf4ad0af093f8858d7090
Aryan Prince <aryanprincesanty@icloud.com>
2023-02-28 00:20:54 +0000

    feat: Built frontend user interface (D3-154)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-function App() {
+import DurationRadioButtons from './components/DurationRadioButtons'
+
+const App = () => {
   return (
-    <div className="min-h-[400px] w-[600px] bg-[#262626] text-white">
+    <div className="min-h-[300px] w-[250px] bg-[#262626] text-white">
       <div className="m-4 flex flex-col gap-4">
-        <p className="text-2xl font-semibold">Sample Extension</p>
-        {/* Additional code goes here */}
+        <p className="text-2xl font-bold">Clear History</p>
+        <DurationRadioButtons />
+        <p className="mt-4 font-semibold text-xs italic">
+          Warning! This will permanently clear your browser history
+        </p>
+        <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
+          Begone history
+        </button>
       </div>
     </div>
   )
---
+++ src/components/DurationRadioButtons.tsx
@@ -0,0 +1,57 @@
+import * as RadioGroupPrimitive from '@radix-ui/react-radio-group'
+import { clsx } from 'clsx'
+import { useState } from 'react'
+
+const durations = [
+  { id: '30min', title: 'Last 30 minutes' },
+  { id: '24hrs', title: 'Last 24 hours' },
+  { id: 'alltime', title: 'All time' },
+]
+
+const RadioGroup = () => {
+  const [value, setValue] = useState(durations[0].title)
+
+  return (
+    <form>
+      <legend className="text-sm font-medium leading-4 text-gray-900 dark:text-gray-100">
+        Select duration to clear:
+      </legend>
+      <RadioGroupPrimitive.Root
+        aria-label="Pokemon starters"
+        defaultValue={'Bulbasaur'}
+        onValueChange={setValue}
+      >
+        <div className="mt-3 space-y-3">
+          {durations.map((duration) => (
+            <div key={duration.id} className="flex items-center">
+              <RadioGroupPrimitive.Item
+                id={duration.id}
+                value={duration.title}
+                className={clsx(
+                  'peer relative w-4 h-4 rounded-full',
+                  // Setting the background in dark properly requires a workaround (see css/tailwind.css)
+                  'border border-transparent text-white',
+                  'radix-state-checked:bg-blue-600',
+                  'radix-state-unchecked:bg-gray-100 dark:radix-state-unchecked:bg-neutral-900',
+                  'focus:outline-none focus:ring-0 focus:ring-offset-0 focus-visible:ring focus-visible:ring-blue-500 focus-visible:ring-opacity-75 focus-visible:ring-offset-2 dark:focus-visible:ring-offset-gray-800'
+                )}
+              >
+                <RadioGroupPrimitive.Indicator className="absolute inset-0 flex items-center justify-center leading-0">
+                  <div className="w-1.5 h-1.5 rounded-full bg-white"></div>
+                </RadioGroupPrimitive.Indicator>
+              </RadioGroupPrimitive.Item>
+              <label
+                htmlFor={duration.id}
+                className="ml-2 block text-sm font-medium text-gray-700 dark:text-gray-300"
+              >
+                {duration.title}
+              </label>
+            </div>
+          ))}
+        </div>
+      </RadioGroupPrimitive.Root>
+    </form>
+  )
+}
+
+export default RadioGroup
